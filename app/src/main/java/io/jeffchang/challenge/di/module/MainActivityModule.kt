072c6303cf37e99886a2ac2beaf4405b0f09ace6
Jeffrey Chang <me@jeffchang.io>
2019-03-24 15:44:49 -0700

    Moved Activity to giphy package

--- app/src/main/java/io/jeffchang/challenge/di/module/MainActivityModule.kt
+++ app/src/main/java/io/jeffchang/challenge/di/module/MainActivityModule.kt
@@ -3,7 +3,7 @@ package io.jeffchang.challenge.di.module
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import io.jeffchang.giphy.GiphyModule
-import io.jeffchang.challenge.MainActivity
+import io.jeffchang.giphy.GiphySearchActivity
 
 @Module
 abstract class MainActivityModule {
@@ -11,6 +11,6 @@ abstract class MainActivityModule {
     @ContributesAndroidInjector(modules = [
         GiphyModule::class
     ])
-    abstract fun contributeMainActivity(): MainActivity
+    abstract fun contributeMainActivity(): GiphySearchActivity
 
 }
--- app/src/main/java/io/jeffchang/giphy/GiphySearchActivity.kt
+++ app/src/main/java/io/jeffchang/giphy/GiphySearchActivity.kt
@@ -1,14 +1,21 @@
-package io.jeffchang.challenge
+package io.jeffchang.giphy
 
 import android.os.Bundle
+import android.view.Menu
 import androidx.navigation.Navigation.findNavController
 import dagger.android.support.DaggerAppCompatActivity
+import io.jeffchang.challenge.R
 
-class MainActivity : DaggerAppCompatActivity() {
+class GiphySearchActivity : DaggerAppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_giphy_search)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.giphy_search_options_menu, menu)
+        return true
     }
 
     override fun onSupportNavigateUp() =
